4b66894878a49579e4965bfe47deffef81d59485
Lao Phan <22121468@STR-Student-LNX-SRV.westernsydney.edu.au>
2025-05-29 04:24:28 +1000

    server connection files

---
+++ backend/includes/connect_db.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbc = mysqli_connect("localhost", "22121468", "Comeflywithme0163", "db_22121468");
+
+// Check connection
+if (!$dbc) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
