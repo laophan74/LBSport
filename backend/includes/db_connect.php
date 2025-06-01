aae20aed88cdfcab074540cb55dba9cda6a9efaf
BobSung22142552 <22142552@student.westernsydney.edu.au>
2025-06-01 23:51:31 +1000

    db_connect update

--- backend/includes/db_connect.php
+++ backend/includes/db_connect.php
@@ -4,4 +4,13 @@
     if ($conn->connect_error) {
         die("Database connection error: " . $conn->connect_error);
     }
+
+    /* use this if not working
+    $conn = new mysqli("localhost", "22142552", "wtbobdb!", "db_22122552");
+
+    if ($conn->connect_error) {
+        die("Database connection error: " . $conn->connect_error);
+    }
+    */
 ?>
+
